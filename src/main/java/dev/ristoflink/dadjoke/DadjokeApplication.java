60a910fa4ce5656eded1c19e5b6a5db9b65ba3a6
RistoFlink <kenssi@gmail.com>
2023-09-15 21:54:32 +0300

    Initial commit

---
+++ src/main/java/dev/ristoflink/dadjoke/DadjokeApplication.java
@@ -0,0 +1,13 @@
+package dev.ristoflink.dadjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadjokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadjokeApplication.class, args);
+	}
+
+}
