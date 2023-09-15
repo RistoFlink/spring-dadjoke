1ab6031ea5bca58822791a61a8daf6e68bdf5f7e
RistoFlink <kenssi@gmail.com>
2023-09-15 23:22:01 +0300

    Finalize the app

--- src/main/java/dev/ristoflink/dadjoke/commands/DadJokeCommands.java
+++ src/main/java/dev/ristoflink/dadjoke/commands/DadJokeCommands.java
@@ -1,7 +1,22 @@
 package dev.ristoflink.dadjoke.commands;
 
+import dev.ristoflink.dadjoke.client.DadJokeClient;
+import dev.ristoflink.dadjoke.model.DadJokeResponse;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
 public class DadJokeCommands {
+
+    private final DadJokeClient dadJokeClient;
+
+    public DadJokeCommands(DadJokeClient dadJokeClient) {
+        this.dadJokeClient = dadJokeClient;
+    }
+
+    @ShellMethod(key = "random", value = "I tell a random dad joke.")
     public String getRandomDadJoke() {
-        return "";
+        DadJokeResponse random = dadJokeClient.random();
+        return random.joke();
     }
 }
